d369f2b1332dd76e87eca463552c28e3a811c42c
Max Livingston <liv.max02@gmail.com>
2024-09-21 15:45:42 +0600

    Finished the task

--- src/main/java/com/attractor/onlineshop/repositories/ProductRepository.java
+++ src/main/java/com/attractor/onlineshop/repositories/ProductRepository.java
@@ -3,6 +3,7 @@ package com.attractor.onlineshop.repositories;
 import com.attractor.onlineshop.entities.Product;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.math.BigDecimal;
@@ -13,7 +14,13 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
     Product findById(long id);
     List<Product> findByName(String name, Pageable pageable);
     List<Product> findByDescription(String description, Pageable pageable);
+    @Query("select p " +
+            "from Product as p " +
+            "where p.price = :price")
     List<Product> findByPrice(BigDecimal price);
+    @Query("select p " +
+            "from Product as p " +
+            "where p.price >= :lowerPrice and p.price <= :upperPrice")
     List<Product> findByPriceBetween(BigDecimal lowerPrice, BigDecimal upperPrice);
     List<Product> findByBrandName(String brandName, Pageable pageable);
     List<Product> findByCategoryName(String categoryName, Pageable pageable);
